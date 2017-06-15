05c830a4736fac5d44d0214ca323c3ba01b57413
Matt McDonald <mattmcd1221@gmail.com>
2017-06-15 15:56:44 -0400

    style changes

--- main.js
+++ main.js
@@ -2,56 +2,6 @@
 // 2. Loop over the data
 // 3. Display it in the `.customers` element
 
-
-  // 'use strict';
-
-  // fetch('https://randomuser.me/api/')
-  // .then(
-  //   function(response) {
-  //     if (response.status !== 200) {
-  //       console.log('Looks like there was a problem. Status Code: ' + response.status);
-  //       return;
-  //   }
-  //   response.json().then(function(data) {
-  //     console.log(data.results[0].name.title);
-  //             // let picture = data.results[0].picture.large;
-
-
-// let people = data.results;
-// function randomDiv() {
-//   for(let i in people) {
-//     let picture = data.results[i].picture.large;
-//   }
-
-
-// let picture = data.results[0].picture.large;
-//
-//   let markup = `
-//     <h1>Internal Company Directory</h1>
-//
-//     <div class="random-container">
-//
-//       <div class="random-block">${randomDiv(people)}</div>
-//       <div class="random-block"><img src="${picture}"></div>
-//       <div class="random-block"><img src="${picture}"></div>
-//       <div class="random-block"><img src="${picture}"></div>
-//       <div class="random-block"><img src="${picture}"></div>
-//       <div class="random-block"><img src="${picture}"></div>
-//       <div class="random-block"><img src="${picture}"></div>
-//       <div class="random-block"><img src="${picture}"></div>
-//       <div class="random-block"><img src="${picture}"></div>
-//       <div class="random-block"><img src="${picture}"></div>
-//       <div class="random-block"><img src="${picture}"></div>
-//       <div class="random-block"><img src="${picture}"></div>
-//
-//
-//     </div>
-//
-//   `
-//   document.body.innerHTML = markup;
-//
-// }})})
-
   // Your Code Goes Here
 
   for (i = 0; i < 12; i++){
